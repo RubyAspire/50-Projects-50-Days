4a243416e5aebd70f2b06749f3d08ff3d18630d6
Keenan <dancemanji@gmail.com>
2021-03-16 04:35:52 +0200

    Added Sound Board

---
+++ Sound Board/script.js
@@ -0,0 +1,44 @@
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+
+
+
+sounds.forEach(sound => {
+    
+	const button = document.createElement("button");
+    
+	button.innerText = sound;
+    
+	button.classList.add("btn");
+    
+    
+	button.addEventListener("click", () => {
+        
+		stopSongs();
+        
+		document.getElementById(sound).play();
+    
+	});
+    
+    
+	document.getElementById("buttons").appendChild(button);
+    
+
+});
+
+
+
+
+function stopSongs(){
+    
+	sounds.forEach(sound => {
+        
+		const song = document.getElementById(sound);
+        
+        
+		song.pause();
+        
+		song.currentTime = 0;
+    
+	});
+
+}
